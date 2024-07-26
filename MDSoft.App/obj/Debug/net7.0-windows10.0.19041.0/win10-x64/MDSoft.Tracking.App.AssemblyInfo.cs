f91932240802dfb484f1ca570d55e40facd7e6e2
Enriquillo Mañon <emanon@agilisa.com>
2024-07-26 05:57:30 -0400

    Merge NET8 Android

--- MDSoft.App/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/MDSoft.Tracking.App.AssemblyInfo.cs
+++ MDSoft.App/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/MDSoft.Tracking.App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MDSoft.Tracking.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fbf2fa739f7afa22ed6d200d285ea361e7bb0626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+20d3a31a0a8219c8ba9ff41e14ceea0e1c0eaf93")]
 [assembly: System.Reflection.AssemblyProductAttribute("MDSoft.Tracking.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MDSoft.Tracking.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
